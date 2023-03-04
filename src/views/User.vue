aaaa460669657e316ef032a38045182660e7ec9e
linecomment <3062876335@qq.com>
2023-03-04 15:12:01 +0800

    commit

--- src/views/User.vue
+++ src/views/User.vue
@@ -22,8 +22,8 @@
         </el-form-item>
         <el-form-item label="性别" prop="gender">
           <el-select v-model="form.gender" placeholder="请选择">
-            <el-option label="男" value="1"></el-option>
-            <el-option label="女  " value="0"></el-option>
+            <el-option label="男" :value="1"></el-option>
+            <el-option label="女  " :value="0"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="出生日期" prop="birth">
@@ -31,6 +31,7 @@
             v-model="form.birth"
             type="date"
             placeholder="选择日期时间"
+            value-format="yyyy-MM-DD"
           >
           </el-date-picker>
         </el-form-item>
@@ -44,33 +45,52 @@
       </span>
     </el-dialog>
     <div class="manage-header">
-      <el-button @click="dialogVisible = true" type="primary">+ 新增</el-button>
+      <el-button @click="handleAdd" type="primary">+ 新增</el-button>
+      <el-form :inline="true" :model="userForm">
+        <el-form-item>
+          <el-input placeholder="请输入名称" v-model="userForm.name"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">查询</el-button>
+        </el-form-item>
+      </el-form>
     </div>
     <!-- 用户表格 -->
-    <el-table :data="tableData" style="width: 100%">
-      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-      <el-table-column prop="age" label="年龄" width="180">
-      </el-table-column>
-      <el-table-column prop="gender" label="性别">
-        <template slot-scope="scope">
-          <span style="margin-left: 10px">{{ scope.row.gender === 1 ? '男' : '女' }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column prop="birth" label="出生日期"> </el-table-column>
-      <el-table-column prop="addr" label="地址"> </el-table-column>
-      <el-table-column prop="addr" label="操作">
-        <template slot-scope="scope">
-          <el-button size="mini" type="primary" @click="handleEdit(scope.raw)">编辑</el-button>
-          <el-button size="mini" type="danger" @click="handleDelete(scope.raw)">删除</el-button>
-        </template>
-      </el-table-column>
-
-    </el-table>
+    <div class="common-table">
+      <el-table stripe :data="tableData" style="width: 100%" height="90%">
+        <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+        <el-table-column prop="age" label="年龄" width="180">
+        </el-table-column>
+        <el-table-column prop="gender" label="性别">
+          <template slot-scope="scope">
+            <span style="margin-left: 10px">{{ scope.row.gender == 1 ? '男' : '女' }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="birth" label="出生日期"> </el-table-column>
+        <el-table-column prop="addr" label="地址"> </el-table-column>
+        <el-table-column prop="addr" label="操作">
+          <template slot-scope="scope">
+            <el-button size="mini" type="primary" @click="handleEdit(scope.row)">编辑</el-button>
+            <el-button size="mini" type="danger" @click="handleDelete(scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div class="pager">
+        <!-- 分页 -->
+        <el-pagination
+          layout="prev, pager, next"
+          :total="total"
+          @current-change="handlePage"
+        >
+        </el-pagination>
+      </div>
+    </div>
+   
   </div>
 </template>
 
 <script>
-import { getUser } from "@/api";
+import { getUser,addUser,editUser,deleteUser } from "@/api";
 export default {
   data() {
     return {
@@ -90,7 +110,15 @@ export default {
         addr: [{ required: true, message: "请输入地址" }],
       },
       tableData: [],
-      modalType:0
+      modalType:0, // 0 表示新增的弹窗，1 表示编辑的弹窗
+      total:0,
+      pageData:{
+        page:1,
+        limit:10
+      },
+      userForm:{
+        name:''
+      }
     }
   },
   methods: {
@@ -101,29 +129,94 @@ export default {
       this.$refs.form.validate((valid) => {
         // vaild 为 true 表示校验通过
         if (valid) {
-          this.dialogVisible = false;
-          this.handleClose();
+          if(this.modalType === 0){
+            addUser(this.form).then(() => {
+              this.getList()
+            })
+          }else {
+            editUser(this.form).then(() => {
+              this.getList()
+            })
+          }
+          this.dialogVisible = false
+          this.handleClose()
         }
-      });
+      })
     },
     cancle() {
       this.handleClose();
+      this.dialogVisible = false
     },
-    handleEdit(val){
-      console.log(val,'val')
+    handleEdit(row){
+      this.modalType = 1
+      this.dialogVisible = true
+      this.form = JSON.parse(JSON.stringify(row))
     },
-    handleDelete(val){
-      console.log(val,'val') 
+    handleDelete(row){
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          deleteUser({ id:row.id }).then(() => {
+            this.$message({
+              type: 'success',
+              message: '删除成功!'
+            });
+            this.getList()
+          })
+          
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });          
+        });
+    },
+    // 获取用户列表数据
+    getList(){
+      getUser({params:{...this.userForm,...this.pageData}}).then(({ data }) => {
+        // console.log(data, "data");
+        this.tableData = data.list
+        this.total = data.count || 0
+     })
+    },
+    handleAdd(){
+      this.modalType = 0
+      this.dialogVisible = true
+    },
+    // 根据页码进行切换
+    handlePage(val){
+      this.pageData.page = val
+      this.getList()
+    },
+    // 列表查询
+    onSubmit(){
+      this.getList()
     }
   },
   mounted() {
-    getUser().then(({ data }) => {
-      // console.log(data, "data");
-      this.tableData = data.list
-    })
+    this.getList()
   },
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.user-container{
+  height: 90%; 
+  .manage-header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .common-table{
+    position: relative;
+    height: calc(100% - 62px);
+    .pager{
+      position: absolute;
+      bottom: 0;
+      right: 20px;
+    }
+  }
+}
 </style>
